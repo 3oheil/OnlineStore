a0292334ab3feca21a4f6299142d978f26442862
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-05-12 09:11:04 -0800

    creare DbContext, add dbSet and Configuration relation with Fluent Api

---
+++ OnlineStore.Infrastructure/Persistence/OnlineStoreDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.Infrastructure.Persistence
+{
+    public class OnlineStoreDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+        public OnlineStoreDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Order>()
+                .HasMany(o => o.Items)
+                .WithOne(i => i.Order)
+                .HasForeignKey(i => i.OrderId);
+
+            modelBuilder.Entity<OrderItem>()
+                .HasOne(i => i.Product)
+                .WithMany()
+                .HasForeignKey(i => i.ProductId);
+        }
+    }
+}
