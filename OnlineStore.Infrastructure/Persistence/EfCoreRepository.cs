0916ec780621d256d9cf9f295f12eb96eef5483e
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-05-12 09:11:44 -0800

    impliment interface

---
+++ OnlineStore.Infrastructure/Persistence/EfCoreRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore.Application.Repositories;
+
+namespace OnlineStore.Infrastructure.Persistence
+{
+    public class EfCoreRepository<T> : IRepository<T> where T : class
+    {
+        private readonly OnlineStoreDbContext _context;
+
+        public EfCoreRepository(OnlineStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.AddAsync(entity);
+        }
+
+        public Task<List<T>> GetAllAsync()
+        {
+            return _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(long id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
