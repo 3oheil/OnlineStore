7d06df82d1cc5fe5577f8500462e227eea7f7f41
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-05-11 20:24:09 -0800

    Design Product Model

---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using OnlineStore.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Domain.Entities
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string? Title { get; set; }
+        public decimal Price { get; set; }
+        public ProductType Type { get; set; }
+    }
+}
