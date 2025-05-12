b5da7667d66705b9ee0266e9fbcdc1f32ae537b4
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-05-12 11:22:14 -0800

    design order controller

---
+++ OnlineStore.API/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Application.Interfaces;
+using OnlineStore.Application.Models;
+
+namespace OnlineStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PlaceOrder([FromBody] OrderDto order)
+        {
+            try
+            {
+                await _orderService.PlaceOrderAsync(order);
+                return Ok("Order placed successfully");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
